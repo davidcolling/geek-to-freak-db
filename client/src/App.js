14d3b98b6966deba645839851dd22d06850e2b8b
David Colling <d@dcolling.com>
2022-11-28 19:41:22 +0000

    Add GeekToFreakDB code

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,70 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class GeekToFreakDB extends React.Component {
+	render() {
+  		return <GeekToFreakWorkoutAdder />
+	}
+}
+
+class GeekToFreakWorkoutAdder extends React.Component {
+	render() {
+		return (
+			<div>
+				<MovementSelect id="movementSelector" />
+				<RepsCountEntry />
+				<WeightEntry />
+			</div>
+		);
+	}
+}
+
+class MovementSelect extends React.Component {
+	render() {
+		return (
+			<select id={this.props.id}>
+				<option value="incline_press">Incline Press</option>
+				<option value="decline_press">Decline Press</option>
+				<option value="pec_fly">Pec Fly</option>
+				<option value="rear_deltoid">Rear Deltoid</option>
+				<option value="hip_abduction">Hip Abduction</option>
+				<option value="hip_adduction">Hip Adduction</option>
+				<option value="squat">Squat</option>
+			</select>
+		);
+	}
+}
+
+class RepsCountEntry extends React.Component {
+	render() {
+		return (
+			<input type="number" min="0" max="8"/>
+		);
+	}
+}
+
+class WeightEntry extends React.Component {
+	render() {
+		return (
+			<div>
+				<input type="number" min="0" max="5000" />
+				<select>
+					<option value="pounds">Pounds</option>
+					<option value="kilograms">Kilograms</option>
+				</select>
+			</div>
+		);
+	}
+}
+
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+	    <GeekToFreakDB />
     );
   }
 }
 
 export default App;
+
