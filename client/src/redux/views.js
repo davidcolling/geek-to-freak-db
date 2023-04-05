bef89eb0b91398e15fed7029e1b300b338a43e0d
David Colling <d@dcolling.com>
2023-04-05 00:39:34 +0000

    Define serialized view strings

--- client/src/redux/views.js
+++ client/src/redux/views.js
@@ -1,1 +1,7 @@
+export const HOME_VIEW = "HOME_VIEW";
+export const WORKOUT_ADDER_VIEW = "WORKOUT_ADDER_VIEW";
+export const EQUIPMENT_VIEW = "EQUIPMENT_VIEW";
+export const EQUIPMENT_ADDER_VIEW = "EQUIPMENT_ADDER_VIEW";
+export const POSTED_VIEW = "POSTED_VIEW";
+export const SET_ADDER_VIEW = "SET_ADDER_VIEW";
 
