cdb21e31c192acca2cdcab04f63fb19efaf3fd6f
David Colling <d@dcolling.com>
2023-04-27 19:28:01 +0000

    Make footer wide enough

--- client/src/WorkoutDB.js
+++ client/src/WorkoutDB.js
@@ -33,8 +33,8 @@ function WorkoutDB ({view, home}) {
                     {
                         position: "fixed",
                         bottom: "0",
-                        width: "90%",
                         display: "block",
+                        width: "100%",
                         margin: "auto",
                         backgroundColor: "white",
                         opacity: "90%"
