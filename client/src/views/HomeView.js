d7a610276c2f747a1d770870ac4823fae0b4abba
David Colling <d@dcolling.com>
2023-05-01 18:53:42 +0000

    Remove test Popup

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import {WideButton, Centered} from '../Elements.js';
 
-export function HomeView({viewWorkoutAdder, viewEquipmentView, launchPopup}) {
+export function HomeView({viewWorkoutAdder, viewEquipmentView}) {
     return (
         <div>
             <Centered height={"600px"} >
                 <WideButton onClick={viewWorkoutAdder} >Add Workout</WideButton>
                 <br />
                 <WideButton onClick={viewEquipmentView} >Equipment</WideButton>
-                <br />
-                <WideButton onClick={launchPopup} >Test PopUp</WideButton>
             </Centered>
         </div>
     );
