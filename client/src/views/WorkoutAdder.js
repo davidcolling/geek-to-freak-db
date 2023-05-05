3d1ca22717584823be852f3c86175b744f7888c7
David Colling <d@dcolling.com>
2023-05-05 19:48:38 +0000

    Display more set info

--- client/src/views/WorkoutAdder.js
+++ client/src/views/WorkoutAdder.js
@@ -4,7 +4,12 @@ import {P, WideButton} from '../elements/elements.js';
 export const WorkoutAdder = ({getCurrentWorkoutConnected, addSet}) => {
     return (
         <div>
-            {getCurrentWorkoutConnected.sets.map( (item) => (<P>{item.equipment} x {item.reps}</P>) )} 
+            {getCurrentWorkoutConnected.sets.map( 
+                (item) => {
+                    var lastRepComplete = item.lastRepComplete ? "" : "to failure";
+                    return <P>{item.equipment} x {item.reps}: {item.weight} lbs {lastRepComplete} </P>
+                }
+            )} 
             <br />
             <WideButton onClick={addSet}> Add Set </WideButton>
         </div>
