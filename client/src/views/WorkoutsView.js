bcba8f4161d2add66e2874ffe31c22f60ed6e9e8
David Colling <d@dcolling.com>
2023-07-12 14:52:31 +0000

    List set movements with better formatting

--- client/src/views/WorkoutsView.js
+++ client/src/views/WorkoutsView.js
@@ -4,19 +4,28 @@ import {WideButton, RightAlign, Bar, P} from '../elements/elements.js';
 export const WorkoutsView = ({list, fetchSetsForWorkoutConnected}) => {
     return (
         <div>
-            {list.map( 
+            {list().map( 
                 (item) => (
-                    <Bar>
-                        <button onClick={() => fetchSetsForWorkoutConnected(item.id)}></button>
-                        <P>{item.startTime}</P>
-                        <RightAlign>
-                            <Bar>
-                                {item.endTime}
-                            </Bar>
-                        </RightAlign>
-                        <P>{JSON.stringify(item.set1)}</P>
-                    </Bar>
-                )
+                    <div>
+                        <Bar>
+                            <button onClick={() => fetchSetsForWorkoutConnected(item.id)}></button>
+                            <P>{item.startTime}</P>
+                            <RightAlign>
+                                <Bar>
+                                    {item.endTime}
+                                </Bar>
+                            </RightAlign>
+                       </Bar>
+                        {
+                            typeof item.set1 === 'object' &&
+                            item.set1.map(
+                                (set) => (
+                                    <P>{set.movement}</P>
+                                )
+                            )
+                        }
+                    </div>
+                 )
             )}
             <br />
         </div>
