b8e35064add8bc20efe1393eb4af16535deff930
David Colling <d@dcolling.com>
2023-07-29 22:46:46 +0200

    Remove use of spacers in higher-level elements

--- client/src/elements/SetList.js
+++ client/src/elements/SetList.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {ShadeBG, P} from './elements.js';
+import {Spacer, ShadeBG, P} from './elements.js';
 
 export const SetList = ({list}) => {
     return (
-        <ShadeBG darkMultiple={1}>
-            {
-                list.map(
-                    (set) => (
-                        <P>{set.movement} X {set.reps} at {set.weight}</P>
+        <div>
+            <ShadeBG darkMultiple={1}>
+                {
+                    list.map(
+                        (set) => (
+                            <P>{set.movement} X {set.reps} at {set.weight}</P>
+                        )
                     )
-                )
-            }
-        </ShadeBG>
+                }
+            </ShadeBG>
+            <Spacer />
+        </div>
     )
 }
 
--- client/src/elements/Workout.js
+++ client/src/elements/Workout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Spacer, RightAlign, Bar, P} from './elements.js';
+import {RightAlign, Bar, P} from './elements.js';
 import SetList from './SetList.js';
 
 export const Workout = ({workout, fetchSetsForWorkoutConnected}) => {
@@ -17,7 +17,6 @@ export const Workout = ({workout, fetchSetsForWorkoutConnected}) => {
                 typeof workout.sets !== 'undefined' && 
                 <div>
                     <SetList list={workout.sets} />
-                    <Spacer />
                 </div>
             }
         </div>
