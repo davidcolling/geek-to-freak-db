732cae8207c60c558911345d505e68462681e28e
David Colling <d@dcolling.com>
2023-07-29 22:22:28 +0200

    Use SetList inside WorkoutAdder Setlist expects movement and db gives equipment

--- client/src/views/WorkoutAdder.js
+++ client/src/views/WorkoutAdder.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {P, WideButton} from '../elements/elements.js';
+import SetList from '../elements/SetList.js';
 
 export const WorkoutAdder = ({getCurrentWorkoutConnected, addSet, post}) => {
     return (
         <div>
             <WideButton onClick={addSet}> Add Set </WideButton>
-            {getCurrentWorkoutConnected.sets.map( 
-                (item) => {
-                    var lastRepComplete = item.lastRepComplete ? "" : "to failure";
-                    return <P>{item.equipment} x {item.reps}: {item.weight} lbs {lastRepComplete} </P>
-                }
-            )} 
+            <SetList list={getCurrentWorkoutConnected.sets} />
             <WideButton onClick={post}> Add Workout </WideButton>
         </div>
     )
