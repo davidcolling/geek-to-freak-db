0e40b982749e26958941b2dd17a7561997ba47b6
David Colling <d@dcolling.com>
2023-08-01 22:22:38 +0200

    Add Spacer to Workou Previous Commmit is erroneously named and im too lazy to fix

--- client/src/elements/Workout.js
+++ client/src/elements/Workout.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import {RightAlign, Bar, P, ShadeBG} from './elements.js';
+import {RightAlign, Bar, P, ShadeBG, Spacer} from './elements.js';
 import SetList from './SetList.js';
 import removeImg from '../images/remove-square.svg';
 
 export const Workout = ({workout, fetchSetsForWorkoutConnected}) => {
     var dm = 1;
     return (
-        <ShadeBG darkMultiple={dm}>
-            <Bar>
-                <P onClick={() => fetchSetsForWorkoutConnected(workout.id)}>{workout.startTime}</P>
-                <RightAlign>
-                    <Bar>
-                        <img src={removeImg} alt="remove" onClick={() => {}} />
-                    </Bar>
-                </RightAlign>
-            </Bar>
-            {
-                typeof workout.sets !== 'undefined' && 
-                <div>
-                    <SetList list={workout.sets} darkMultiple={dm +1}/>
-                </div>
-            }
-        </ShadeBG>
+        <div>
+            <ShadeBG darkMultiple={dm}>
+                <Bar>
+                    <P onClick={() => fetchSetsForWorkoutConnected(workout.id)}>{workout.startTime}</P>
+                    <RightAlign>
+                        <Bar>
+                            <img src={removeImg} alt="remove" onClick={() => {}} />
+                        </Bar>
+                    </RightAlign>
+                </Bar>
+                {
+                    typeof workout.sets !== 'undefined' && 
+                    <div>
+                        <SetList list={workout.sets} darkMultiple={dm +1}/>
+                    </div>
+                }
+            </ShadeBG>
+            <Spacer />
+        </div>
     )
 }
 
